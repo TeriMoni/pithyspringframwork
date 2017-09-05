12a478d296f6920ab37cc38c365f58257670012b
TeriMoni <351111617@qq.com>
2017-09-05 14:03:13 +0800

    bean帮助类

---
+++ src/main/java/helper/BeanHelper.java
@@ -0,0 +1,49 @@
+package helper;
+
+import utils.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Autor: bin.liu
+ * @Desciption: bean帮助类
+ * @Date: Created in 13:51 2017/9/5
+ */
+public class BeanHelper {
+
+    /**
+     * 定义bean映射关系（用于存放bean类和bean实例的映射关系）
+     */
+    public static final Map<Class<?>,Object> BEN_MAP = new HashMap<Class<?>,Object>();
+
+    static {
+        Set<Class<?>> beanSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass:beanSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEN_MAP.put(beanClass,obj);
+        }
+    }
+
+    /**
+     * 获取bean映射
+     * @return
+     */
+    public static Map<Class<?>,Object> getBenMap(){
+        return  BEN_MAP;
+    }
+
+    /**
+     * 根据类获取bean实例
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<?> cls){
+        if(!BEN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not find bean by class:"+cls);
+        }
+        return (T) BEN_MAP.get(cls);
+    }
+}

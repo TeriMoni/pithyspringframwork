59e93ff6811d76816950101391a7cda2df444f03
TeriMoni <351111617@qq.com>
2017-09-05 17:11:21 +0800

    程序主入口，用来加载helper类

---
+++ src/main/java/HelpLoader.java
@@ -0,0 +1,19 @@
+import helper.BeanHelper;
+import helper.ClassHelper;
+import helper.ControllerHelper;
+import helper.IocHelper;
+import utils.ClassUtil;
+
+/**
+ * @Autor: bin.liu
+ * @Desciption: 加载helper类，程序初始化入口
+ * @Date: Created in 17:04 2017/9/5
+ */
+public final class HelpLoader {
+    public static void init(){
+        Class<?>[] classList = {ClassHelper.class, BeanHelper.class, IocHelper.class, ControllerHelper.class};
+        for (Class<?> cls: classList) {
+            ClassUtil.loadClass(cls.getName(),false);
+        }
+    }
+}

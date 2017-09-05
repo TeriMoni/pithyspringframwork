aed5728d0cfa46ba33084a3fd2042828850c270e
TeriMoni <351111617@qq.com>
2017-09-05 18:07:05 +0800

    返回数据对象

---
+++ src/main/java/bean/Data.java
@@ -0,0 +1,19 @@
+package bean;
+
+/**
+ * @Autor: bin.liu
+ * @Desciption: 返回数据对象
+ * @Date: Created in 18:06 2017/9/5
+ */
+public class Data {
+
+    private Object model;
+
+    public Data(Object model) {
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+}

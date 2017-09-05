507c61917bceced1838c35378c205e59e2afe447
TeriMoni <351111617@qq.com>
2017-09-05 16:32:45 +0800

    请求bean

---
+++ src/main/java/bean/Request.java
@@ -0,0 +1,53 @@
+package bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * @Autor: bin.liu
+ * @Desciption: 请求bean
+ * @Date: Created in 16:17 2017/9/5
+ */
+public class Request {
+
+    /**
+     * 请求方法
+     */
+    private String requestMethod;
+
+    /**
+     * 请求路径
+     */
+    private String requestPath;
+
+    public Request(String requestMethod, String requestPath) {
+        this.requestMethod = requestMethod;
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public void setRequestPath(String requestPath) {
+        this.requestPath = requestPath;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+       return EqualsBuilder.reflectionEquals(this,obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}

8f36ef8a00bd3de22e10d5c76ef6a64a3be318cf
TeriMoni <351111617@qq.com>
2017-09-11 17:26:19 +0800

    控制器注解

--- src/main/java/annotation/Controller.java
+++ src/main/java/annotation/Controller.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * @Autor: bin.liu
  * @Desciption: ¿ØÖÆÆ÷×¢½â
- * @Date: Created in 11:52 2017/9/5
+ * @Date: Created in 11:52 2017/9/8
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
